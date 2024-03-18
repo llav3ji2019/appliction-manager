ceb37aa177d10051894c5cc033357b94a9526660
llav3ji2019 <pavelemelyanov03@mail.ru>
2024-03-18 04:54:27 +0300

    add logout

--- src/main/java/com/llav3ji2019/application/applicationmanager/config/SecurityConfig.java
+++ src/main/java/com/llav3ji2019/application/applicationmanager/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
@@ -67,7 +68,11 @@ public class SecurityConfig {
                 )
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout -> logout
+                        .logoutUrl("/auth/v1/logout")
+                        .addLogoutHandler(new SecurityContextLogoutHandler())
+                );
         return http.build();
     }
 }
